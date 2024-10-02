0428a1cd1980ac362cc7ca42bc422ef8cc53152b
Jack Arnott <jack.arnott002@gmail.com>
2024-10-02 17:46:39 +0100

    linitng changed

--- FretboardTrainer/Trainer.py
+++ FretboardTrainer/Trainer.py
@@ -8,16 +8,17 @@ from scipy import fftpack
 import numpy as np
 import librosa
 import pyaudio
-import matplotlib.pyplot as plt
 class FretboardNoteTrainer:
-
+    """
+    _summary_
+    """
     def __init__(self):
 
         _loc = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         with open(os.path.join(_loc,'configs.json'),'r',encoding='utf-8') as file:
-            self.configs = json.load(file)        
+            self.configs = json.load(file)
 
-        self.notes = {'A', 'A#', 'B', 'C', 'C#', 'D', 'E', 'F', 'F#', 'G', 'G#', 'A'}
+        self.notes = {'A', 'A#', 'B', 'C', 'C#', 'D', 'E', 'F', 'F#', 'G', 'G#'}
         self.attempts = 0
         self.record_seconds = self.configs['RecordSeconds']
         self.pyaud = None # TODO - test to see if pyaudio class can be initialised once
@@ -30,23 +31,22 @@ class FretboardNoteTrainer:
 
         except KeyError:
             print('Notes completed')
-            pass
-        
+
         print(f'Play {correct_note}')
         return correct_note
 
 
-    def get_frequency(self, signal) -> tuple[np.ndarray,np.ndarray]: #TODO - determine how to figure out sample rate
+    def get_frequency(self, signal) -> tuple[np.ndarray,np.ndarray]:
         """
         Obtains the frequency values for a given signal and sample rate
 
         :return: the frequency and magnitude arrays of the signal
         """
-        signal_points = len(self.signal)
-        time = 1.0 / self.sample_rate
-        signal_array = self.signal[:signal_points]
+        signal_points = len(signal)
+        time = 1.0 / self.configs['Rate']
+        signal_array = signal[:signal_points]
         fft_signal = fftpack.fft(signal_array)
-        freq_array = fftpack.fftfreq(signal_points, time)[:signal_points//2]    
+        freq_array = fftpack.fftfreq(signal_points, time)[:signal_points//2]
         magnitude_array = 2.0 / signal_points * np.abs(fft_signal[:signal_points // 2])
         return freq_array, magnitude_array
 
@@ -57,13 +57,14 @@ class FretboardNoteTrainer:
 
         :return: Recorded signal
         """
-        chunk = self.configs['Chunk']   
-        format = pyaudio.paInt16
-        channels = 1 if sys.platform == 'darwin' else 2
+        chunk = self.configs['Chunk']
         rate = self.configs['Rate']
+        formats = pyaudio.paInt16
+        channels = 1 if sys.platform == 'darwin' else 2
+
         pyaud = pyaudio.PyAudio()
 
-        stream = pyaud.open(format=format, channels=channels, rate=rate, input=True)
+        stream = pyaud.open(format=formats, channels=channels, rate=rate, input=True)
         frames = []
 
         print('Recording...')
@@ -76,12 +77,11 @@ class FretboardNoteTrainer:
         stream.stop_stream()
         stream.close()
         pyaud.terminate()
-
         return waveform
 
 
     def freq_to_note(self, freq_array: np.ndarray, magnitude_array: np.ndarray) -> str: #TODO - Update method
-        """Determines the Note played from the Frequency"""
+        """Determines the note played from the Frequency"""
         note_frequency = freq_array[np.where(magnitude_array == np.max(magnitude_array))[0]]
         return librosa.hz_to_note(note_frequency)[:-1]
 
@@ -96,23 +96,23 @@ class FretboardNoteTrainer:
         """Function which listens for note and returns audio array"""
         played_note = self.determine_note_main(signal)
         if played_note == correct_note:
-            print('Correct')
-            self.attempts += 1
             return True
-        print('Incorrect, try again')
         return False
 
 
     def main(self) -> None:
+        """
+        Main function to run
+        """
         while self.notes:
             correct_note = self.generate_note()
             while True:
                 played_note = self.record_note()
+                self.attempts +=1
                 if self.evaluate_note(correct_note, played_note):
                     print('Correct')
                     break
-                else:
-                    print('Incorrect, Try again')
+                print('Incorrect, Try Again')
         print(f'\nComplete! \nAttempts: {self.attempts} \nScore: {12/self.attempts*100:.2f}%')
 
 
