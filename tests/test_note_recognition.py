a18a5a5b3f8ba954bfe2e2f4b56de81dfb3804f7
Jack Arnott <jack.arnott002@gmail.com>
2024-10-02 22:26:17 +0100

    added filepath compatability

--- tests/test_note_recognition.py
+++ tests/test_note_recognition.py
@@ -1,7 +1,13 @@
+import sys
 import unittest
-# from fretboard_trainer import FretboardNoteTrainer
+sys.path.append('..')
+from FretboardTrainer.fretboard_trainer import FretboardNoteTrainer
 
 class TestNote(unittest.TestCase):
     
     def test_note(self):
-        pass
+        print('test')
+        trainer = FretboardNoteTrainer()
+        
+        
+#TODO - Write unit tests
