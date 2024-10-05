b8bdffdad20c7f8ff476fe67c47afe511326db2c
Jack Arnott <jack.arnott002@gmail.com>
2024-10-05 15:28:42 +0100

    removed variable

--- Modules/utils.py
+++ Modules/utils.py
@@ -4,7 +4,6 @@ import argparse
 def parse_args() -> argparse.Namespace:
     """Argument Parser function"""
     parser = argparse.ArgumentParser(description='TeacherAI - A virtual guitar practice tool')
-    parser.add_argument('-hm','--hardmode', help='Hard Mode On', required=False)
-    args = parser.parse_args()
-    return args
+    parser.add_argument('-hm','--hardmode', help='Hard Mode On', action='store_true',default =False, required=False)
+    return parser.parse_args()
     
