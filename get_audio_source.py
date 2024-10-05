cc8cb7ca78f4f04552f5f1f6243598235afa7628
Jack Arnott <jack.arnott002@gmail.com>
2024-10-05 15:59:40 +0100

    variable name change

--- get_audio_source.py
+++ get_audio_source.py
@@ -9,9 +9,9 @@ def get_audio_source() -> None:
     pyaudio = PyAudio()
     info = pyaudio.get_host_api_info_by_index(0)
     numdevices = info.get('deviceCount')
-    for i in range(0, numdevices):
-        if (pyaudio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print("Input Device id ", i, " - ", pyaudio.get_device_info_by_host_api_device_index(0, i).get('name'))
+    for device in range(0, numdevices):
+        if (pyaudio.get_device_info_by_host_api_device_index(0, device).get('maxInputChannels')) > 0:
+            print("Input Device id ", device, " - ", pyaudio.get_device_info_by_host_api_device_index(0, device).get('name'))
 
 if __name__ == '__main__':
     get_audio_source()
