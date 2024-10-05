619a527d15b7dfe3cd73e6fa549495e6a8d4f26d
Jack Arnott <jack.arnott002@gmail.com>
2024-10-05 14:23:39 +0100

    small variable changes

--- get_audio_source.py
+++ get_audio_source.py
@@ -2,16 +2,16 @@
 Script to display the avaliable audio sources
 """
 
-import pyaudio
+from pyaudio import PyAudio
 
 def get_audio_source() -> None:
     """Displays audio sources and their IDs"""
-    p = pyaudio.PyAudio()
-    info = p.get_host_api_info_by_index(0)
+    pyaudio = PyAudio()
+    info = pyaudio.get_host_api_info_by_index(0)
     numdevices = info.get('deviceCount')
     for i in range(0, numdevices):
-        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+        if (pyaudio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", pyaudio.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 if __name__ == '__main__':
     get_audio_source()
